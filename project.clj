cd2e0193f43b02ab45328555dfdaa52f4a2bf021
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-01-16 17:30:12 +0200

    upgrade deps

--- project.clj
+++ project.clj
@@ -2,18 +2,18 @@
   :description "FIXME: write description"
   :url "http://github.com/opqdonut/clojure-train-radiator"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [cheshire "5.8.0"]
-                 [clj-http "3.7.0"]
-                 [clj-time "0.14.0"]
-                 [compojure "1.5.1"]
+                 [cheshire "5.8.1"]
+                 [clj-http "3.9.1"]
+                 [clj-time "0.15.1"]
+                 [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-defaults "0.3.2"]
                  [ring-server "0.5.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler handler/app}
   :repl-options {:init-ns handler}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.2"]]}})
